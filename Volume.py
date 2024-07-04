215dd50049ae46dc03122f5771d408a621060a8f
Ashu Choudhary <ashuchoudhary7983@gmail.com>
2024-07-04 18:12:12 +0530

    Add explanation comments

--- Volume.py
+++ Volume.py
@@ -10,7 +10,7 @@ drawing_utils = mp.solutions.drawing_utils #And the drawing_utils can capture th
 while True:
 
     _, image = webcam.read() #webcam.read() function return 2 statement first is status of webcam and second is the image OR frame captured
-    image = cv2.flip(image, 1)
+    image = cv2.flip(image, 1) #This flip function
     frame_height, frame_width, _ = image.shape
     rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #The rgb_image object is convert the captured image from BGR to RGB
     output = my_hands.process(rgb_image) #In this output object we can process our hands with the help of my_hands variable and RGB_Converted image
@@ -18,32 +18,32 @@ while True:
 
     if hands:
         for hand in hands:
-            drawing_utils.draw_landmarks(image, hand)
+            drawing_utils.draw_landmarks(image, hand)# It draws points in our hands
             landmarks = hand.landmark
             for id, landmark in enumerate(landmarks):
                 x = int(landmark.x * frame_width)
                 y = int(landmark.y * frame_height)
 
                 if id == 8:
-                    cv2.circle(img=image, center=(x, y), radius=8, color=(0, 255, 255), thickness=3)
+                    cv2.circle(img=image, center=(x, y), radius=8, color=(0, 0, 0), thickness=5) # It can create a circle in my index finger of black color
                     x1 = x
                     y1 = y
                 if id == 4:
-                    cv2.circle(img=image, center=(x, y), radius=8, color=(0, 0, 255), thickness=3)
+                    cv2.circle(img=image, center=(x, y), radius=8, color=(0, 0, 255), thickness=5)# It can create a circle in my thumb of red color
                     x2 = x
                     y2 = y
 
-        dist = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5 // 4
-        cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 5)
+        dist = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5 // 4 #Calculate the distance between our finger and the thumb
+        cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 5)# Make a line between our finger and the thumb
         if dist > 20:
-            pyautogui.press("volumeup")
+            pyautogui.press("volumeup")#It can increase the volume, if the calculated distance is greater then 20
         else:
-            pyautogui.press("volumedown")
+            pyautogui.press("volumedown")#It can Decrease the volume, if the calculated distance is smaller then 20
 
     cv2.imshow("Hand Based volume Control", image) #cv2.imshow have to show the captured image in the read() function above
     key = cv2.waitKey(10) #cv2.waitkey function is used to wait for next turn of while loop. I passed 10 milliseconds
 
-    if key == 27:# '27' is the 'Esc' key, if user press 'Esc' key the capture video function was destroyed
+    if key == 27:# '27' is the 'Esc' key, if user press 'Esc' key the programs is closed
         break
-webcam.release()
-cv2.destroyAllWindows()
+webcam.release()#It can release the webcam
+cv2.destroyAllWindows()#This is to close all the running windows
