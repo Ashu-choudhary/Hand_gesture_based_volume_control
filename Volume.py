39523e1a73807c03b2fc3f96dc5faa5c784366ff
Ashu Choudhary <ashuchoudhary7983@gmail.com>
2024-07-04 13:32:28 +0530

    comments edit

--- Volume.py
+++ Volume.py
@@ -4,34 +4,16 @@ import pyautogui
 
 x1 = y1 = x2 = y2 = 0
 
-''' 
-webcam is an object for Video Capture function form CV2 library And passing an integer parameter as 0,1,2,
-...because how many number of webcam you have 0 for one, 1 for two and like that. 
-'''
-webcam = cv2.VideoCapture(0)
-
-'''In my_hands variable can capture hands movements'''
-
-my_hands = mp.solutions.hands.Hands()
-
-'''And the drawing_utils can capture the points in our hands in all figures and thumb'''
-
-drawing_utils = mp.solutions.drawing_utils
+webcam = cv2.VideoCapture(0) #webcam is an object for Video Capture function form CV2 library And passing an integer parameter as 0,1,2,...because how many number of webcam you have 0 for one, 1 for two and like that.
+my_hands = mp.solutions.hands.Hands() #In my_hands variable can capture hands movements
+drawing_utils = mp.solutions.drawing_utils #And the drawing_utils can capture the points in our hands in all figures and thumb
 while True:
 
-    '''webcam.read() function return 2 statement first is status of webcam and second is the image OR frame captured '''
-
-    _, image = webcam.read()
+    _, image = webcam.read() #webcam.read() function return 2 statement first is status of webcam and second is the image OR frame captured
     image = cv2.flip(image, 1)
     frame_height, frame_width, _ = image.shape
-
-    '''The rgb_image object is convert the captured image from BGR to RGB'''
-
-    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    '''In this output object we can process our hands with the help of my_hands variable and RGB_Converted image'''
-
-    output = my_hands.process(rgb_image)
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #The rgb_image object is convert the captured image from BGR to RGB
+    output = my_hands.process(rgb_image) #In this output object we can process our hands with the help of my_hands variable and RGB_Converted image
     hands = output.multi_hand_landmarks
 
     if hands:
@@ -58,17 +40,10 @@ while True:
         else:
             pyautogui.press("volumedown")
 
-    '''cv2.imshow have to show the captured image in the read() function above'''
+    cv2.imshow("Hand Based volume Control", image) #cv2.imshow have to show the captured image in the read() function above
+    key = cv2.waitKey(10) #cv2.waitkey function is used to wait for next turn of while loop. I passed 10 milliseconds
 
-    cv2.imshow("Hand Based volume Control", image)
-
-    '''cv2.waitkey function is used to wait for next turn of while loop. I passed 10 milliseconds'''
-
-    key = cv2.waitKey(10)
-
-    ''' '27' is the 'Esc' key, if user press 'Esc' key the capture video function was destroyed'''
-
-    if key == 27:
+    if key == 27:# '27' is the 'Esc' key, if user press 'Esc' key the capture video function was destroyed
         break
 webcam.release()
 cv2.destroyAllWindows()
